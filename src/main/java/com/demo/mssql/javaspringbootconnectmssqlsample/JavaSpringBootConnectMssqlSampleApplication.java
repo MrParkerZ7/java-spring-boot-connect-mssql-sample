558a9a3274167361be2a004085e463b7929c5702
MrParkerZ7 <neverzero9@gmail.com>
2019-06-06 23:45:13 +0700

    [Initial]

---
+++ src/main/java/com/demo/mssql/javaspringbootconnectmssqlsample/JavaSpringBootConnectMssqlSampleApplication.java
@@ -0,0 +1,13 @@
+package com.demo.mssql.javaspringbootconnectmssqlsample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaSpringBootConnectMssqlSampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaSpringBootConnectMssqlSampleApplication.class, args);
+	}
+
+}
